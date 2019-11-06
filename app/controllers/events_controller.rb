f79c5812fbb93bbafe00e23b6c10fce302de100f
LucasJego <jego.lucas@gmail.com>
2019-11-06 01:25:58 +0100

    Attention, peinture fraîche.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,12 +17,25 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
 	end
 
-  #
+  # Méthode qui va créer le nouvel événement à partir du formulaire de 'new.html.erb' soumis par l'utilisateur
+  # Le contenu de ce formulaire est accessible via le hash 'params'
+  # A la création, l'utilisateur est redirigé vers la page 'show' de l'événement
   def create
-  	@city = ['Ouagadougou', 'Montpellier']
-  	puts '-'*60
-  	puts @city
-  	puts '*' * 60
+  	puts 'a' *60
   	puts params
+  	puts 's'*68
+  	@event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location], administrator_id: current_user.id)
+
+    # Si l'événement a pu être sauvegardé dans la BDD, i.e. si @event.save est true
+  	if @event.save
+      flash[:success] = "Ton Evenement a été créé !"
+      redirect_to events_path
+    # Sinon
+    else
+      flash.now[:danger] = "Ton evenement n'a pas été créé"
+      render :new
+    end
+
+  	#redirect_to 
   end
 end
